528c638500d0288ddad6512d765f35bac70313cf
drjasonnichols <43385425+drjasonnichols@users.noreply.github.com>
2024-09-25 19:27:34 -0700

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    # Basic configuration for the Flask application
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://cis440_template:cis440_template@107.180.1.16:3306/cis440_template'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '<a word or phrase used to encrypt>'  # You should replace this with a stronger key in production
