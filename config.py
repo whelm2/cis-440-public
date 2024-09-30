37f5c374efbb3433169498bdcc8339d44de70d7a
drjasonnichols <jrn8969@gmail.com>
2024-09-30 11:05:33 -0700

    in class branch

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     # Basic configuration for the Flask application
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<username>:<password>@<ip of db>:<port>/<schema>'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '<a word or phrase used to encrypt>'  # You should replace this with a stronger key in production
